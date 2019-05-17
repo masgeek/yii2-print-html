f30eb57e40d129093b6e67df59da05c904e6391a
kanioxnia <50703406+kanioxnia@users.noreply.github.com>
2019-05-17 10:45:27 +0530

    Update PrintThisAsset.php

--- PrintThisAsset.php
+++ PrintThisAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class PrintThisAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/yii2assets/yii2-print-html/assets';
+    public $sourcePath = '@vendor/yii2assets/yii2-print-html';
 
     public $js = [
         '//code.jquery.com/jquery-migrate-1.3.0.min.js',
