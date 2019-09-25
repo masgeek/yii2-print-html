f11b9bfdaa18449fb6d69b8cdb0461fd5af29c26
Sammy Barasa <barsamms@gmail.com>
2019-09-25 09:50:21 +0300

    Updated package dependencies

--- PrintThisAsset.php
+++ PrintThisAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class PrintThisAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/yii2assets/yii2-print-html';
+    public $sourcePath = '@bower/printthis';
 
     public $js = [
         '//code.jquery.com/jquery-migrate-1.3.0.min.js',
